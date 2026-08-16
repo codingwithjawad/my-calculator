822a533231d4280d6183618ba5be087dbec364bb
codingwithjawad <jawad4hmad@gmail.com>
2026-08-16 11:26:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let temp;
+let fnd = document.querySelector('#fnd');
+let equal = document.querySelector('.equal-btn');
+let btn = document.querySelector('.btn-container');
+btn.addEventListener('click', function(button){
+	if(button.target.value == 'c')
+				fnd.value = ''
+	else if(button.target.value =='='){
+		fnd.value = eval(fnd.value)
+		temp = fnd.value;
+	}
+	else if(button.target.value =='ans')
+		fnd.value += temp;
+	else if(button.target.value =='<')
+		fnd.value = fnd.value.slice(0,-1)
+	else
+		fnd.value += button.target.value;
+})
